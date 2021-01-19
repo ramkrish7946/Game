6f66350de0820f73d8a96a2d132834d6baf964c0
ramakrishna7946 <Ramakrishnananduri35@gmail.com>
2021-01-19 22:31:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gameproject/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.gameproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private gameview gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        gameView = new gameview(this, point.x, point.y);
+
+        setContentView(gameView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+}
---
+++ app/src/main/java/com/example/gameproject/background.java
@@ -0,0 +1,17 @@
+package com.example.gameproject;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+class Background {
+    int x=0,y=0;
+    Bitmap background;
+
+     Background (int screenX, int screenY, Resources res)  {
+        background = BitmapFactory.decodeResource(res,R.drawable.background2);
+        background = Bitmap.createScaledBitmap(background, screenX,screenY,false);
+
+
+    }
+}
