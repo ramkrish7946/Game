24d6177942120597f6a24345a888dddd85a18f47
ramkrish7946 <76998733+ramkrish7946@users.noreply.github.com>
2021-01-21 12:37:39 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/gameproject/MainActivity.java
+++ app/src/main/java/com/example/gameproject/MainActivity.java
@@ -24,7 +24,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+        
+        @Override
+public void onBackPressed() {
+   Intent intent = new Intent(Intent.ACTION_MAIN);
+   intent.addCategory(Intent.CATEGORY_HOME);
+   intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+   startActivity(intent);
+ }
     }
+    
 
 
 
